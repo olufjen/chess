db008a7c081445c0b3c068f2784b6fdd3de9c939
olufjen <olufjen@gmail.com>
2021-01-28 09:35:59 +0100

    Restoration

--- src/main/java/no/chess/web/model/game/KnowledgeBuilder.java
+++ src/main/java/no/chess/web/model/game/KnowledgeBuilder.java
@@ -1,6 +1,14 @@
 
 package no.chess.web.model.game;
 
+import java.util.List;
+
+import aima.core.logic.fol.Connectors;
+import aima.core.logic.fol.kb.FOLKnowledgeBase;
+import aima.core.logic.fol.kb.data.Literal;
+import aima.core.logic.fol.parsing.ast.ConnectedSentence;
+import aima.core.logic.fol.parsing.ast.Predicate;
+import aima.core.logic.planning.Utils;
 import no.games.chess.AbstractGamePiece.pieceType;
 
 /**
@@ -34,9 +42,16 @@ public class KnowledgeBuilder {
   private static String KING =  "KING";
   private static String QUEEN =  "QUEEN";
   private static String OCCUPIES = "occupies";
+  private static String PAWNATTACK = "PAWNATTACK";
   
   
-  public static String getOCCUPIES() {
+  public static String getPAWNATTACK() {
+	return PAWNATTACK;
+}
+public static void setPAWNATTACK(String pAWNATTACK) {
+	PAWNATTACK = pAWNATTACK;
+}
+public static String getOCCUPIES() {
 	return OCCUPIES;
 }
 public static void setOCCUPIES(String oCCUPIES) {
@@ -50,218 +65,181 @@ public static String getPLAY()
   {
 		PLAY = pLAY;
   }
-
   public static String getPAWN()
   {
 		return PAWN;
   }
-
   public static void setPAWN(String pAWN)
   {
 		PAWN = pAWN;
   }
-
   public static String getKNIGHT()
   {
 		return KNIGHT;
   }
-
   public static void setKNIGHT(String kNIGHT)
   {
 		KNIGHT = kNIGHT;
   }
-
   public static String getBISHOP()
   {
 		return BISHOP;
   }
-
   public static void setBISHOP(String bISHOP)
   {
 		BISHOP = bISHOP;
   }
-
   public static String getROOK()
   {
 		return ROOK;
   }
-
   public static void setROOK(String rOOK)
   {
 		ROOK = rOOK;
   }
-
   public static String getKING()
   {
 		return KING;
   }
-
   public static void setKING(String kING)
   {
 		KING = kING;
   }
-
   public static String getQUEEN()
   {
 		return QUEEN;
   }
-
   public static void setQUEEN(String qUEEN)
   {
 		QUEEN = qUEEN;
   }
-
   public static String getPAWNMOVE()
   {
 		return PAWNMOVE;
   }
-
   public static void setPAWNMOVE(String pAWNMOVE)
   {
 		PAWNMOVE = pAWNMOVE;
   }
-
   public static String getPIECETYPE()
   {
 		return PIECETYPE;
   }
-
   public static void setPIECETYPE(String pIECETYPE)
   {
 		PIECETYPE = pIECETYPE;
   }
-
   public static String getACTION()
   {
 		return ACTION;
   }
-
   public static void setACTION(String aCTION)
   {
 		ACTION = aCTION;
   }
-
   public static String getPROTECTED()
   {
 		return PROTECTED;
   }
-
   public static void setPROTECTED(String pROTECTED)
   {
 		PROTECTED = pROTECTED;
   }
-
   public static String getSimpleProtected()
   {
 		return simpleProtected;
   }
-
   public static void setSimpleProtected(String simpleProtected)
   {
 		KnowledgeBuilder.simpleProtected = simpleProtected;
   }
-
   public static String getATTACKED()
   {
-		return ATTACKED;
+	  return ATTACKED;
   }
-
   public static void setATTACKED(String aTTACKED)
   {
 		ATTACKED = aTTACKED;
   }
-
   public static String getCAPTURE()
   {
 		return CAPTURE;
   }
-
   public static void setCAPTURE(String cAPTURE)
   {
 		CAPTURE = cAPTURE;
   }
-
   public static String getCONQUER()
   {
 		return CONQUER;
   }
-
   public static void setCONQUER(String cONQUER)
   {
 		CONQUER = cONQUER;
   }
-
   public static String getTHREATEN()
   {
 		return THREATEN;
   }
-
   public static void setTHREATEN(String tHREATEN)
   {
 		THREATEN = tHREATEN;
   }
-
   public static String getOWNER()
   {
 		return OWNER;
   }
-
   public static void setOWNER(String oWNER)
   {
 		OWNER = oWNER;
   }
-
   public static String getMOVE()
   {
 		return MOVE;
   }
-
   public static void setMOVE(String mOVE)
   {
 		MOVE = mOVE;
   }
-
   public static String getREACHABLE()
   {
 		return REACHABLE;
   }
-
   public static void setREACHABLE(String rEACHABLE)
   {
 		REACHABLE = rEACHABLE;
   }
-
   public static String getCANMOVE()
   {
 		return CANMOVE;
   }
-
   public static void setCANMOVE(String cANMOVE)
   {
 		CANMOVE = cANMOVE;
   }
-
   public static String getSAFEMOVE()
   {
 		return SAFEMOVE;
   }
-
   public static void setSAFEMOVE(String sAFEMOVE)
   {
 		SAFEMOVE = sAFEMOVE;
   }
-
   public static String getSTRIKE()
   {
 		return STRIKE;
   }
-
   public static void setSTRIKE(String sTRIKE)
   {
 		STRIKE = sTRIKE;
   }
-
-  public static String getPieceType(AgamePiece piece) {
+  /**
+   * getPieceType
+   * This method returns the string type of the piece
+ * @param piece
+ * @return
+ */
+public static String getPieceType(AgamePiece piece) {
 	  pieceType type = piece.getPieceType();
 		if (type == type.PAWN) {
 			return PAWN;
@@ -283,4 +261,21 @@ public static String getPLAY()
 		}	
 	  return null;
   }
+
+public static void parseSentence(String sentence,String goalSentence,FOLKnowledgeBase kb){
+	List<Literal> rules = Utils.parse(sentence);
+	List<Literal> goals = Utils.parse(goalSentence);
+	ConnectedSentence premise = null;
+	int s = rules.size();
+	for (int i= 0; i < s;i=i+2) {
+		Predicate p = (Predicate) rules.get(i).getAtomicSentence();
+		Predicate y = (Predicate) rules.get(i+1).getAtomicSentence();	
+		premise = new ConnectedSentence(Connectors.AND,p,y);
+
+	}
+	Predicate g = (Predicate)goals.get(0).getAtomicSentence();
+	ConnectedSentence goal = new ConnectedSentence(Connectors.IMPLIES,premise,g);
+	kb.tell(goal);
+
+}
 }
